d4ca72ee791066d986388e02e6da2cbc43cee7ff
chippermist <chinmaygarg@me.com>
2019-11-20 19:26:25 +0000

    updated integer overflow

--- tests/integer_overflow_standalone/ioflow.rkt
+++ tests/integer_overflow_standalone/ioflow.rkt
@@ -12,33 +12,43 @@
 
 (require "generated/racket/test/ioflow.ll.rkt")
 
-(define A #xFFFFFFFF)
-(define B #xFFFFFFFF)
-(define C #x1FFFFFFFE)
+;(define A (bv #xFFFFFFFF 32))
+;(define B (bv #xFFFFFFFF 32))
+(define C (bv #x1FFFFFFFE 32))
+(define A (bv 5 32))
+(define B (bv 4 32))
 
-(define-symbolic x y (bitvector 32))
+;(define-symbolic x y (bitvector 32))
 
 ; racket implementation
-
 (define (ioflow_test_1 val1 val2)
+  (displayln "Starting native test...")
   (define r (bv 0 32))
-  (set! r (bvadd val1 val2))
+  (set! r (bvadd (bv 2 32) (bv 3 32)))
+  (displayln "Setting r to (bvadd x y)...")
 r)
 
 ; define the function to check
-
 (define (check-ioflow-test-1)
+  (displayln "Starting test...")
+  (define-symbolic x (bitvector 32))
+  (define-symbolic y (bitvector 32))
+  (define pre (ioflow_test_1 x y))
+  (displayln "Defined pre...")
+;  (define (val) (@test (bv 3 32) (bv 4 32)))
   (define (val) (@test x y))
-  (displayln "solving..")
-  (check-unsat? (verify (bveq (ioflow_test_1 x y) val))))
-
-
-
-
+  (val)
+  (displayln "Defined asserted...")
+  (define post (ioflow_test_1 x y))
+  (displayln "Defined post...")
+  ;(for-each (lambda (A B) (check-unsat? (verify (assert A B)))) asserted)
+  (check-unsat? (verify (assert (implies pre post))))
+  (displayln "Passed assert (pre == post)...")
+  (displayln "Solving...")
+  (check-unsat? (verify (assert (equal? (ioflow_test_1 A B) (val))))))
 
 
 ; define test
-
 (define ioflow-tests
   (test-suite+
     "Test for Integer Overflow ioflow.c"
