a8144f5267aa656bf8d65d56cdfbbde923ccb1a7
chippermist <chinmaygarg@me.com>
2019-12-03 03:08:27 +0000

    changed mutex test

--- tests/locking_test/mutex.c
+++ tests/locking_test/mutex.c
@@ -3,35 +3,55 @@
 uint32_t lock;
 uint32_t count;
 
+/**
+  * Set the value of count
+  **/
 void set_count_value(uint32_t x) {
     count = x;
 }
 
+/**
+  * Get the value of count
+  **/
 uint32_t get_count_value(void) {
     return count;
 }
 
+/**
+  * Set the value of lock
+  **/
 void set_lock_value(uint32_t x) {
     lock = x;
 }
 
+/**
+  * Get the value of lock
+  **/
 uint32_t get_lock_value(void) {
     return lock;
 }
 
+/**
+  * Set
+  **/
 int32_t increment_count(uint32_t x) {
-  if(lock != 0) return -1;
+  if(get_lock_value() != 0) return -1;
   lock++;
   count = count+x;
-  // while(1) {}
+  // while(x) {}
   lock--;
   return 0;
 }
 
+/**
+  * Get
+  **/
 int32_t get_count(void) {
-  if(lock != 0) return -1;
+  // this is because it cannot detect deadlocks using wait() 
+  if(get_lock_value() != 0) return -1;
+  int x;
   lock++;
-  int x = count;
+  x = count;
   lock--;
-  return 0;
+  return x;
 }
