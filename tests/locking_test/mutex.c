38b4ae9d385766526015a53d49b33ae110b2516f
chippermist <chinmaygarg@me.com>
2019-12-03 01:52:16 +0000

    added mutex wait

--- tests/locking_test/mutex.c
+++ tests/locking_test/mutex.c
@@ -3,19 +3,35 @@
 uint32_t lock;
 uint32_t count;
 
+void set_count_value(uint32_t x) {
+    count = x;
+}
+
+uint32_t get_count_value(void) {
+    return count;
+}
+
+void set_lock_value(uint32_t x) {
+    lock = x;
+}
+
+uint32_t get_lock_value(void) {
+    return lock;
+}
+
 int32_t increment_count(uint32_t x) {
   if(lock != 0) return -1;
   lock++;
   count = count+x;
-  while(1) {}
+  // while(1) {}
   lock--;
   return 0;
 }
 
-int32_t get_count(uint32_t x) {
+int32_t get_count(void) {
   if(lock != 0) return -1;
   lock++;
-  x = count;
+  int x = count;
   lock--;
   return 0;
 }
